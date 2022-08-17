1c73d2333c8008cb9b1860b2b0720ca0d93691af
RafaIzu <bomizukawa@gmail.com>
2022-08-17 20:57:52 -0300

    first commit

---
+++ flask_qa/settings.py
@@ -0,0 +1,7 @@
+import os 
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
